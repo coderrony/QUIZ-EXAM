104f2212f47dfdcf66e44d6d496f745bf672b7f5
Rony Das <69383115+coderrony@users.noreply.github.com>
2025-08-15 11:42:43 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    images: {
+    domains: [process!.env!.NEXT_PUBLIC_APP_URL!],
+       remotePatterns: [
+ {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+      },
+    ]
+    }
 };
 
 export default nextConfig;
